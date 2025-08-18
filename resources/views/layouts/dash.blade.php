85d06eeaca7471563879214b350ec932f90b4384
blueswayne133 <blueswayne133@gmail.com>
2025-08-18 14:59:55 -0700

    update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -9,29 +9,43 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $settings->site_name }} | @yield('title')</title>
     @section('styles')
-        <link rel="icon" href="{{ asset('storage/app/public/' . $settings->favicon) }}" type="image/png" />
-        <!-- Font Awesome 5 -->
-        <link rel="stylesheet" href="{{ asset('dash2/libs/%40fortawesome/fontawesome-pro/css/all.min.css') }}">
-        <!-- Page CSS -->
-        <link rel="stylesheet" href="{{ asset('dash2/libs/fullcalendar/dist/fullcalendar.min.css') }}">
-        <!-- Purpose CSS -->
-        <link rel="stylesheet" href="{{ asset('dash2/css/' . $settings->website_theme) }}" id="stylesheet">
-        <link rel="stylesheet" href="{{ asset('dash2/libs/animate.css/animate.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('dash2/libs/sweetalert2/dist/sweetalert2.min.css') }}">
-        <script src="{{ asset('dash2/libs/sweetalert/sweetalert.min.js') }} "></script>
-        <link rel="stylesheet" type="text/css"
-            href="https://cdn.datatables.net/v/bs4/dt-1.10.21/af-2.3.5/b-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/r-2.2.5/datatables.min.css" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
+    <link rel="icon" href="{{ asset('storage/app/public/' . $settings->favicon) }}" type="image/png" />
+    <!-- Font Awesome 5 -->
+    <link rel="stylesheet" href="{{ asset('dash2/libs/%40fortawesome/fontawesome-pro/css/all.min.css') }}">
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="{{ asset('dash2/libs/fullcalendar/dist/fullcalendar.min.css') }}">
+    <!-- Purpose CSS -->
+    <link rel="stylesheet" href="{{ asset('dash2/css/' . $settings->website_theme) }}" id="stylesheet">
+    <link rel="stylesheet" href="{{ asset('dash2/libs/animate.css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash2/libs/sweetalert2/dist/sweetalert2.min.css') }}">
+    <script src="{{ asset('dash2/libs/sweetalert/sweetalert.min.js') }} "></script>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/v/bs4/dt-1.10.21/af-2.3.5/b-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/r-2.2.5/datatables.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
 
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ asset('dash2/libs/flatpickr/dist/flatpickr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dash2/libs/flatpickr/dist/flatpickr.min.css') }}">
     @show
     @livewireStyles
+
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+_smartsupp.key = '52146ae80e1279b1d217e7ddbb88fb0370bf8ee5';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </head>
 
-<body class="application application-offset" >
+<body class="application application-offset">
     <script>
         {!! $settings->tawk_to !!}
     </script>
@@ -56,9 +70,9 @@
                             {{ date('Y') }}</p>
                     </div>
                     @if ($settings->google_translate == 'on')
-                        <div class="text-right col-sm-6 text-md-center">
-                            <div id="google_translate_element"></div>
-                        </div>
+                    <div class="text-right col-sm-6 text-md-center">
+                        <div id="google_translate_element"></div>
+                    </div>
                     @endif
                 </div>
             </div>
@@ -66,36 +80,36 @@
     </div>
 
     @section('scripts')
-        <!-- Scripts -->
-        <!-- Core JS - includes jquery, bootstrap, popper, in-view and sticky-kit -->
-        <script src="{{ asset('dash2/js/purpose.core.js') }}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-        <!-- Bootstrap Notify -->
-        <script src="{{ asset('dash2/libs/bootstrap-notify/bootstrap-notify.min.js') }} "></script>
-        <!-- Page JS -->
-        <script src="{{ asset('dash2/libs/progressbar.js/dist/progressbar.min.js') }}"></script>
-        <script src="{{ asset('dash2/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-        <script src="{{ asset('dash2/libs/moment/min/moment.min.js') }}"></script>
-        <script src="{{ asset('dash2/libs/fullcalendar/dist/fullcalendar.min.js') }}"></script>
-        <script src="{{ asset('dash2/libs/sweetalert/sweetalert.min.js') }} "></script>
-        <!-- Purpose JS -->
-        <script src="{{ asset('dash2/js/purpose.js') }}"></script>
-        <script type="text/javascript"
-            src="https://cdn.datatables.net/v/bs4/dt-1.10.21/af-2.3.5/b-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/r-2.2.5/datatables.min.js">
-        </script>
-        {{-- my custom javascript file --}}
-        <script src="{{ asset('dash2/libs/flatpickr/dist/flatpickr.min.js') }}"></script>
+    <!-- Scripts -->
+    <!-- Core JS - includes jquery, bootstrap, popper, in-view and sticky-kit -->
+    <script src="{{ asset('dash2/js/purpose.core.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <!-- Bootstrap Notify -->
+    <script src="{{ asset('dash2/libs/bootstrap-notify/bootstrap-notify.min.js') }} "></script>
+    <!-- Page JS -->
+    <script src="{{ asset('dash2/libs/progressbar.js/dist/progressbar.min.js') }}"></script>
+    <script src="{{ asset('dash2/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('dash2/libs/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('dash2/libs/fullcalendar/dist/fullcalendar.min.js') }}"></script>
+    <script src="{{ asset('dash2/libs/sweetalert/sweetalert.min.js') }} "></script>
+    <!-- Purpose JS -->
+    <script src="{{ asset('dash2/js/purpose.js') }}"></script>
+    <script type="text/javascript"
+        src="https://cdn.datatables.net/v/bs4/dt-1.10.21/af-2.3.5/b-1.6.3/b-flash-1.6.3/b-html5-1.6.3/b-print-1.6.3/r-2.2.5/datatables.min.js">
+    </script>
+    {{-- my custom javascript file --}}
+    <script src="{{ asset('dash2/libs/flatpickr/dist/flatpickr.min.js') }}"></script>
 
     @show
-@if($settings->tido)
+    @if($settings->tido)
     <script src="//code.tidio.co/{{$settings->tido}}.js" async></script>
     @endif
-    
-    
+
+
     <!-- GetButton.io widget -->
     @if($settings->whatsapp)
-<script type="text/javascript">
-    (function () {
+    <script type="text/javascript">
+        (function () {
         var options = {
             whatsapp: "{{$settings->whatsapp}}", // WhatsApp number
             call_to_action: "Message us", // Call to action
@@ -107,11 +121,12 @@
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
         var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
     })();
-</script>
-  @endif
-<!-- /GetButton.io widget -->
+    </script>
+    @endif
+    <!-- /GetButton.io widget -->
     <script src="{{ asset('dash2/js/scriptfile.js') }}"></script>
-    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+    <script type="text/javascript"
+        src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
     </script>
 
     <script type="text/javascript">
@@ -128,7 +143,7 @@
         integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous">
     </script>
     @livewireScripts
- 
+
 </body>
 
 </html>
